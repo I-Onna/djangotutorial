14f2d2756d1e5b974e1f5580cc25517d654ecd91
Núria Pujol Vilanova <npujol.py@gmail.com>
2025-06-03 18:13:02 +0200

    afegits resta de fitxers projecte mysite i polls

--- mysite/settings.py
+++ mysite/settings.py
@@ -54,9 +54,9 @@ ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
-        "APP_DIRS": True,
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.request",
